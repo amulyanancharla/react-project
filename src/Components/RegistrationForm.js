9e6477137c31f69c7d40a9d5bc22612ee2f77453
Ravi Teja Varma <ravitejavarma@Ravis-MacBook-Air.local>
2020-06-30 12:09:20 +0530

    newsigninpage

--- src/Components/RegistrationForm.js
+++ src/Components/RegistrationForm.js
@@ -11,34 +11,30 @@ function Registrationform({handleSubmit, submitting}) {
     <Authbackground>
     <form onSubmit={handleSubmit}>
       <Paper style={{ padding : "2rem" }}>
-        <Grid container spacing={2} direction="column"> 
-          <Grid item>
+        <Grid container spacing={2}> 
+          <Grid item container>
             <Typography variant="h5" component="h4">
               Registration Process
             </Typography>
           </Grid>
-          <Grid item container direction="row">
-            <Grid sm={5}>
-              
-              <TextField id="Enter Full Name" label="Enter Full Name" variant="outlined" />
-              <TextField id="Mobile Number" label="Mobile Number" variant="outlined" />
-              <TextField id="Jurisdiction" label="Jurisdiction" variant="outlined" />
-              <TextField id="Password" label="Password" variant="outlined" />
-              <TextField id="ReEnter Password" label="ReEnter Password" variant="outlined" />
-
+          <Grid item xs={6}>
+            <Grid item container direction="column">
+                <TextField id="Enter Full Name" label="Enter Full Name" variant="outlined" />
+                <TextField id="Mobile Number" label="Mobile Number" variant="outlined" />
+                <TextField id="Jurisdiction" label="Jurisdiction" variant="outlined" />
+                <TextField id="Password" label="Password" variant="outlined" />
+                <TextField id="ReEnter Password" label="ReEnter Password" variant="outlined" />
             </Grid>
-          
-            
-          </Grid>
-          <Grid item>
-            <Button
-              variant="contained"
-              color="primary"
-              type="submit"
-              // disabled={submitting || pristine || invalid}
-            >
-              submit
-            </Button>
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                // disabled={submitting || pristine || invalid}
+              >
+                submit
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
       </Paper>
